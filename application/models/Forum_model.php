09b8c32785226636a60df60653834902d6ca03b6
tomacoin <tomacoin@gmail.com>
2015-05-27 22:48:43 -0500

    Models init

---
+++ application/models/Forum_model.php
@@ -0,0 +1,7 @@
+class Forum_model extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ application/models/Guild_model.php
@@ -0,0 +1,7 @@
+class Guild_model extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+}
