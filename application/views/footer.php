5b3e02f9a47d13c68180d3160df7ad6af1bcd830
tomacoin <tomacoin@users.noreply.github.com>
2015-05-31 13:45:37 -0500

    Views

---
+++ application/views/footer.php
@@ -0,0 +1,32 @@
+<footer class="row">
+                <div class="large-12 columns"><hr>
+                    <div class="row">
+                        <div class="large-6 columns">
+                                <p>© Copyright no one at all. Go to town.</p>
+                        </div>
+                        <div class="large-6 small-12 columns">
+                                <ul class="inline-list right">
+                                    <li><a href="#">Link 1</a></li>
+                                    <li><a href="#">Link 2</a></li>
+                                    <li><a href="#">Link 3</a></li>
+                                    <li><a href="#">Link 4</a></li>
+                                </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script>
+    document.write('<script src=<?php echo base_url(); ?>js/vendor/' +
+    ('__proto__' in {} ? 'zepto' : 'jquery') +
+    '.js><\/script>')
+    </script>
+    <script src="<?php echo base_url(); ?>js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>   
+    <div class="reveal-modal-bg" style="display: none;"></div>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Foundation | Welcome</title>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/foundation.css" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/foundation-icons/foundation-icons.css" />
+        <script src="<?php echo base_url(); ?>js/vendor/modernizr.js"></script>
+    </head>
+    <body> 
+
+    <?php include( 'topbar.php' ); ?>
+
+    <div class="row">
+        <div class="large-12 columns">
+            <div class="row">
+                <div class="large-12 hide-for-small">
+                    <img src="http://placehold.it/1200x500&text=Header">
+            </div>
+            </div>
+            <div class="row">
+                <div class="large-12 columns show-for-small">
+                    <img src="http://placehold.it/1200x700&text=Mobile Header">
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12">
+                    <div class="icon-bar five-up">
+                        <a href="" class="item">
+                            <label>Home</label>
+                          </a>
+                          <a href="events" class="item">
+                            <label>Events</label>
+                          </a>
+                          <a href="forum" class="item">
+                            <label>Forum</label>
+                          </a>
+                          <a href="members" class="item">
+                            <label>Members</label>
+                          </a>
+                          <a href="about" class="item">
+                            <label>About</label>
+                          </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <br />
