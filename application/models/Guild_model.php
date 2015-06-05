fdd7c8621ec9e9a762e2adbbf957dc95404a3c68
tomacoin <tomacoin@gmail.com>
2015-06-04 23:12:01 -0500

    Guild and User Models

--- application/models/Guild_model.php
+++ application/models/Guild_model.php
@@ -7,54 +7,81 @@ class Guild_model extends CI_Model {
         parent::__construct();
     }
 
-    function create_guild ( &$data ) {
+    function create_guild ( ) 
+    {
 
-    	$data['owner'];
-    	$data['name'];
-    	$data['motto'];
-    	$data['description'];
-    	$data['game'];
-
-    }
-
-    function change_settings( &$data ) {
-
-    }
-
-    function get_guild ( $gid ) {
+        $data = array(
+            'name' => $this->input->post( 'name' ),
+            'owner' => $this->session->userdata('uid'),
+            'description' => $this->input->post( 'description' ),
+            'game' => $this->input->post( 'game' ),
+        );
+        $this->db->insert( 'users', $data );
 
     }
 
-    function get_settings ( $gid ) {
+    function change_settings( &$data ) 
+    {
 
     }
 
-    function get_members ( $gid ) {
+    function get_guild ( $gid ) 
+    {
+        $query = $this->db->where('gid', $gid)->get();
+
+        if ($query->num_rows() > 0)
+        {
+           $row = $query->row();
+           return $row;
+        }
+    }
+
+    function get_settings ( $gid ) 
+    {
 
     }
 
-    function get_owner ( $gid ) {
+    function get_members ( $gid ) 
+    {
 
     }
 
-    function get_admins ( $gid ) {
+    function get_owner ( $gid ) 
+    {
 
     }
 
-    function get_moderators ( $gid ) {
+    function get_admins ( $gid ) 
+    {
 
     }
 
-    function is_member ( $uid, $gid ) {
+    function get_moderators ( $gid ) 
+    {
 
     }
 
-    function join ( $uid, $gid ) {
+    function is_member ( $uid, $gid ) 
+    {
 
     }
 
-    function leave ( $uid, $gid ) {
+    function join ( $uid, $gid ) 
+    {
+        $data = array(
+            'gid' => $gid,
+            'uid' => $uid
+        );
+        $this->db->insert( 'membership', $data );
+    }
 
+    function leave ( $uid, $gid ) 
+    {
+        $data = array(
+            'gid' => $gid,
+            'uid' => $uid
+        );
+        $this->db->delete( 'membership', $data );
     }
 
     function moderator ( $uid, $gid ) {
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -7,7 +7,7 @@ class User_model extends CI_Model {
         parent::__construct();
     }
 
-    function login($username,$password)
+    function login( $username, $password )
 	{
 		$this->db->where("username",$username);
 		$this->db->where("password",$password);
@@ -32,7 +32,8 @@ class User_model extends CI_Model {
 		return false;
 	}
 
-	function create_user () {
+	function create_user () 
+	{
 		$data = array(
 			'username' => $this->input->post( 'username' ),
 			'email' => $this->input->post( 'email' ),
@@ -41,36 +42,73 @@ class User_model extends CI_Model {
 		$this->db->insert( 'users', $data );
 	}
 
-	function change_password ( $uid, $oldpass, $newpass ) {
+	function change_password ( $uid, $oldpass, $newpass ) 
+	{
+		$this->db->where('uid', $uid);
+		$this->db->where('password', md5( $oldpass ) ); 
+
+		$query = $this->db->get();
+
+		// Check oldpass correct
+		if ($query->num_rows() > 0)
+		{
+			$this->db->where('uid', $uid);
+			$this->db->update('users', array( 'password' => md5( $newpass ) ) ); 
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 
 	}
 
-	function change_email ( $uid, $newemail ) {
+	function change_email ( $uid, $newemail ) 
+	{
+		$this->db->where('uid', $uid);
+		$this->db->update('users', array( 'email' => $newemail ) ); 
+	}
+
+	function edit_profile ( &$data ) 
+	{
 
 	}
 
-	function edit_profile ( &$data ) {
+	function edit_settings ( &$data ) 
+	{
 
 	}
 
-	function edit_settings ( &$data ) {
+	function get_profile ( $uid ) 
+	{
 
 	}
 
-	function get_profile ( $uid ) {
+	function get_settings ( $uid ) 
+	{
 
 	}
 
-	function get_settings ( $uid ) {
+	function get_username ( $uid ) 
+	{
+		$query = $this->db->where('uid', $uid)->get();
 
+		if ($query->num_rows() > 0)
+		{
+		   $row = $query->row();
+		   return $row->username;
+		}
 	}
 
-	function get_username ( $uid ) {
+	function get_uid ( $username ) 
+	{
+		$query = $this->db->where('username', $username)->get();
 
-	}
-
-	function get_uid ( $username ) {
-
+		if ($query->num_rows() > 0)
+		{
+		   $row = $query->row();
+		   return $row->uid;
+		}
 	}
 
 }
