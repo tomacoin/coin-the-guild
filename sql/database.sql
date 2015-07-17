e6d8de21e0a4aaddb22a35b6d6ba132fa51b4dbd
tomacoin <tomacoin@gmail.com>
2015-07-16 22:21:13 -0500

    Added Avatar URL

--- sql/database.sql
+++ sql/database.sql
@@ -1,31 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jul 07, 2015 at 06:10 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `cointheguild`
---
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.17 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.1.0.4867
 -- --------------------------------------------------------
 
---
--- Table structure for table `events`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table cointheguild.events
 CREATE TABLE IF NOT EXISTS `events` (
   `eid` int(11) NOT NULL,
   `gid` int(11) NOT NULL,
@@ -39,30 +24,29 @@ CREATE TABLE IF NOT EXISTS `events` (
   `endtime` time NOT NULL,
   `occurence` int(11) NOT NULL,
   KEY `FK_owner_event` (`owner`),
-  KEY `FK_guild_event` (`gid`)
+  KEY `FK_guild_event` (`gid`),
+  CONSTRAINT `events_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `guilds` (`gid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `events_ibfk_2` FOREIGN KEY (`owner`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `forums`
---
 
+-- Dumping structure for table cointheguild.forums
 CREATE TABLE IF NOT EXISTS `forums` (
   `fid` int(11) NOT NULL AUTO_INCREMENT,
   `gid` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`fid`),
-  UNIQUE KEY `FK_forum_guild` (`gid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  UNIQUE KEY `FK_forum_guild` (`gid`),
+  CONSTRAINT `forums_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `guilds` (`gid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `guilds`
---
 
+-- Dumping structure for table cointheguild.guilds
 CREATE TABLE IF NOT EXISTS `guilds` (
   `gid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
@@ -72,31 +56,30 @@ CREATE TABLE IF NOT EXISTS `guilds` (
   `created` datetime NOT NULL,
   `game` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`gid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `membership`
---
 
+-- Dumping structure for table cointheguild.membership
 CREATE TABLE IF NOT EXISTS `membership` (
   `gid` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
   `role` int(11) NOT NULL DEFAULT '0',
-  `displayas` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `display_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `avatar_url` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `joined` datetime NOT NULL,
   `quit` datetime NOT NULL,
   KEY `FK_guild_user` (`gid`),
-  KEY `FK_user_guild` (`uid`)
+  KEY `FK_user_guild` (`uid`),
+  CONSTRAINT `membership_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `guilds` (`gid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `membership_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `replies`
---
 
+-- Dumping structure for table cointheguild.replies
 CREATE TABLE IF NOT EXISTS `replies` (
   `rid` int(11) NOT NULL AUTO_INCREMENT,
   `tid` int(11) NOT NULL,
@@ -107,15 +90,15 @@ CREATE TABLE IF NOT EXISTS `replies` (
   `original` text NOT NULL,
   PRIMARY KEY (`rid`),
   KEY `FK_reply_user` (`poster`),
-  KEY `FK_reply_thread` (`tid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+  KEY `FK_reply_thread` (`tid`),
+  CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `threads` (`tid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `replies_ibfk_2` FOREIGN KEY (`poster`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `threads`
---
 
+-- Dumping structure for table cointheguild.threads
 CREATE TABLE IF NOT EXISTS `threads` (
   `tid` int(11) NOT NULL AUTO_INCREMENT,
   `fid` int(11) DEFAULT NULL,
@@ -128,63 +111,27 @@ CREATE TABLE IF NOT EXISTS `threads` (
   PRIMARY KEY (`tid`),
   KEY `tid` (`tid`),
   KEY `threads_ibfk_2` (`fid`),
-  KEY `FK_poster_user` (`poster`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  KEY `FK_poster_user` (`poster`),
+  CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`poster`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `threads_ibfk_2` FOREIGN KEY (`fid`) REFERENCES `forums` (`fid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `users`
---
 
+-- Dumping structure for table cointheguild.users
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` int(10) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(254) COLLATE utf8_unicode_ci NOT NULL,
+  `avatar_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `joined` datetime NOT NULL,
   `lastlogin` datetime NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `events`
---
-ALTER TABLE `events`
-  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `guilds` (`gid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `events_ibfk_2` FOREIGN KEY (`owner`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `forums`
---
-ALTER TABLE `forums`
-  ADD CONSTRAINT `forums_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `guilds` (`gid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `membership`
---
-ALTER TABLE `membership`
-  ADD CONSTRAINT `membership_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `guilds` (`gid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `membership_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `replies`
---
-ALTER TABLE `replies`
-  ADD CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `threads` (`tid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `replies_ibfk_2` FOREIGN KEY (`poster`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `threads`
---
-ALTER TABLE `threads`
-  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`poster`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `threads_ibfk_2` FOREIGN KEY (`fid`) REFERENCES `forums` (`fid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
